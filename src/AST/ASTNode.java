c62a3c3e2c4ea34fc6316bd970d2b14b1f3c8dc7
zhangchengkai <31203538+zhangchengkai@users.noreply.github.com>
2023-02-01 21:12:11 +0800

    for codegen

--- src/AST/ASTNode.java
+++ src/AST/ASTNode.java
@@ -1,12 +1,15 @@
 package AST;
 
+import MiddleEnd.BaseClass.Value;
 import Utils.Position;
 
 public abstract class ASTNode{
     private Position pos;
+    public Value IRoperand;
 
     public ASTNode(Position _pos){
         this.pos = _pos;
+        this.IRoperand = null;
     }
 
     public Position getPos(){
